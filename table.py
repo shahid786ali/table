100b23434ec004de3fc78ea97343cb33a8c1c952
shahid786ali <95071441+shahid786ali@users.noreply.github.com>
2022-06-01 02:32:24 -0700

    Add files via upload

---
+++ table.py
@@ -0,0 +1,9 @@
+#This is a simple program that print your requried table
+x= int(input("Please input your requried table number : "))
+
+for z in range(1,21):
+    result=x*z
+   
+    print(f"{x} X {z} = {result}")
+print("#### Good Luck ###")
+ 
